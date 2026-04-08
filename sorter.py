5fe729c26af6c73b9673a736a131340037318ef1
Jan Kulhanek <kulhaja9@fel.cvut.cz>
2026-04-08 20:34:28 +0200

    story

--- sorter.py
+++ sorter.py
@@ -1,12 +1,32 @@
 from protector import sorted
 
 """
-Po dlouhé cestě Jindřich ze Skalice zaskočil do své oblíbené nalévárny. Netrpělivě čekal na svého oblíbeného
-miláčka. Upil si pár piv a jeho pohled 
+Po dlouhé cestě Jindřich ze Skalice zaskočil do své oblíbené nalévárny. Netrpělivě
+tam čekal na svého drahého přítele, pana Ptáčka. Odpil si pár loků piva, když v
+tom jeho pohled upoutal mladý bard.
+
+"Hej, ty! Zahraj mi něco, ať mi ta chvíle rychleji uteče," houkl na něj Jindra.
+"Samo sebou," odvětil bard, "ale nejdřív mi zaplať pivo."
+
+Jindřich pohlédl na mladíka a poté do svého měšce plného zakrvácených grošů. V
+duchu si sice pomyslel: "I'm feeling quite hungry," ale nakonec uznal, že
+se raději nechá nasytit kvalitní hudbou. Otočil se proto zpět k bardovi a hodil
+mu jeden ze špinavých grošů.
+
+Bard se minci pokusil polapit, ale ta mu vyklouzla z dlaní. Začala se nekontro-
+lovaně odrážet po místnosti sem a tam. V tu ránu se stal malér. Bardovi spadly
+všechny noty na zem a beznadějně se pomíchaly. "Že já blbec jsem se to nenaučil
+nazpaměť!" láteřil v duchu hudebník.
+
+"No, tak kde to vázne?" pronesl vážně Jindra a položil ruku na stůl.
+
+Zpocený bard začal zmateně přehrabovat listy papíru. V tu chvíli ho však osvítil
+spásný nápad - naprogramovat řadicí algoritmus, který by za něj listy bleskově
+uspořádal. "To můj problém určitě vyřeší!" zvolal nadšeně.
 """
 
 def tomas_sorter_function(scrambled_list):
 
-    
+
 
     return scrambled_list
